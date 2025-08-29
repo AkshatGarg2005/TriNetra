bea7e722e8c7ce7a96c0ca8cb1a32ab39417b764
Akshat Garg <akshatgarg@Akshats-MacBook-Pro.local>
2025-08-29 15:57:24 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // add all your source files
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(0, 0%, 100%)",
+        foreground: "hsl(222.2, 47.4%, 11.2%)",
+        muted: "hsl(210, 40%, 96.1%)",
+        "muted-foreground": "hsl(215.4, 16.3%, 46.9%)",
+      },
+    },
+  },
+  plugins: [],
+};
